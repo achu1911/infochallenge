451e633230a9ff2b765b5f7f0de51aa59ec5a71a
Alexa Chu <145298611+achu1911@users.noreply.github.com>
2025-03-08 00:54:53 -0500

    Update combining_files.py

--- combined/combining_files.py
+++ combined/combining_files.py
@@ -7,11 +7,9 @@ cleaned_weekends_folder = r'C:\Users\alexa\OneDrive\Documents\GitHub\infochallen
 
 combined_folder = r'C:\Users\alexa\OneDrive\Documents\GitHub\infochallenge\combined'
 
-# Paths for the combined files 
 combined_weekdays_path = os.path.join(combined_folder, 'combined_weekdays.csv')
 combined_weekends_path = os.path.join(combined_folder, 'combined_weekends.csv')
 
-# Function to combine CSV files from a folder
 def combine_csv_files(folder_path, combined_file_path):
     csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
     # List to hold DF
@@ -22,12 +20,11 @@ def combine_csv_files(folder_path, combined_file_path):
         df = pd.read_csv(file_path)
         dfs.append(df)
 
-    # Combine all DataFrames into one DataFrame
+
     combined_df = pd.concat(dfs, ignore_index=True)
     combined_df.to_csv(combined_file_path, index=False)
 
     print(f"Combined CSV saved to: {combined_file_path}")
 
-# Combine and save CSVs for weekdays and weekends
 combine_csv_files(cleaned_weekdays_folder, combined_weekdays_path)
 combine_csv_files(cleaned_weekends_folder, combined_weekends_path)
